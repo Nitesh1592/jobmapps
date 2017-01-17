cdf23aa9f0b88611db3468560bcc93474c39ee15
Nitesh1592 <ndubey501@gmail.com>
2017-01-17 12:03:40 +0530

    Adding all folder and files first time

---
+++ app/example.js
@@ -0,0 +1,172 @@
+var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
+app.factory("services", ['$http', function($http) {
+  var serviceBase = 'services/'
+    var obj = {};
+    obj.getCustomers = function(){
+        return $http.get(serviceBase + 'customers');
+    }
+    obj.getCustomer = function(customerID){
+        return $http.get(serviceBase + 'customer?id=' + customerID);
+    }
+
+    obj.login = function (rowdata) {
+    return $http.post(serviceBase + 'login', rowdata).then(function (results) {
+        return results;
+    });
+	};
+
+	obj.updateCustomer = function (id,customer) {
+	    return $http.post(serviceBase + 'updateCustomer', {id:id, customer:customer}).then(function (status) {
+	        return status.data;
+	    });
+	};
+
+	obj.deleteCustomer = function (id) {
+	    return $http.delete(serviceBase + 'deleteCustomer?id=' + id).then(function (status) {
+	        return status.data;
+	    });
+	};
+
+    return obj;   
+}]);
+
+app.controller('homeCtrl', function ($scope, services) {
+//    $scope.slider = {
+//    options: {
+//        start: function (event, ui) { $log.info('Slider start'); },
+//        stop: function (event, ui) { $log.info('Slider stop'); }
+//        }
+//    }
+//    $scope.dologin = function(data) {
+//      $location.path('/');
+//        services.login(data);
+//        
+//    };
+});
+
+app.controller('editCtrl', function ($scope, $rootScope, $location, $routeParams, services, customer) {
+    var customerID = ($routeParams.customerID) ? parseInt($routeParams.customerID) : 0;
+    $rootScope.title = (customerID > 0) ? 'Edit Customer' : 'Add Customer';
+    $scope.buttonText = (customerID > 0) ? 'Update Customer' : 'Add New Customer';
+      var original = customer.data;
+      original._id = customerID;
+      $scope.customer = angular.copy(original);
+      $scope.customer._id = customerID;
+
+      $scope.isClean = function() {
+        return angular.equals(original, $scope.customer);
+      }
+
+      $scope.deleteCustomer = function(customer) {
+        $location.path('/');
+        if(confirm("Are you sure to delete customer number: "+$scope.customer._id)==true)
+        services.deleteCustomer(customer.customerNumber);
+      };
+
+      $scope.saveCustomer = function(customer) {
+        $location.path('/');
+        if (customerID <= 0) {
+            services.insertCustomer(customer);
+        }
+        else {
+            services.updateCustomer(customerID, customer);
+        }
+    };
+});
+
+app.controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+
+  $scope.options = {
+    customClass: getDayClass,
+    minDate: new Date(),
+    showWeeks: true
+  };
+
+  // Disable weekend selection
+  function disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  }
+
+  $scope.toggleMin = function() {
+    $scope.options.minDate = $scope.options.minDate ? null : new Date();
+  };
+
+  $scope.toggleMin();
+
+  $scope.setDate = function(year, month, day) {
+    $scope.dt = new Date(year, month, day);
+  };
+
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date(tomorrow);
+  afterTomorrow.setDate(tomorrow.getDate() + 1);
+  $scope.events = [
+    {
+      date: tomorrow,
+      status: 'full'
+    },
+    {
+      date: afterTomorrow,
+      status: 'partially'
+    }
+  ];
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  }
+});
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        title: 'Home',
+        templateUrl: 'partials/datepick.html',
+        controller: 'DatepickerDemoCtrl'
+//        controller: 'homeCtrl'
+      })
+      .when('/edit-customer/:customerID', {
+        title: 'Edit Customers',
+        templateUrl: 'partials/edit-customer.html',
+        controller: 'editCtrl',
+        resolve: {
+          customer: function(services, $route){
+            var customerID = $route.current.params.customerID;
+            return services.getCustomer(customerID);
+          }
+        }
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+}]);
+
+app.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
